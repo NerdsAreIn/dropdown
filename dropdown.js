0a33d75d39ce97561f7a96a273fc66f1279b2018
nerds-are-in <annacantthinkofagoodaddress@hotmail.co.uk>
2022-02-12 19:18:30 +0000

    Pretty happy with everything. Now to collapsing and click event handlers.

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,7 @@
-const headers = Array.from(document.getElementsByClassName("header"));
+const links = document.querySelectorAll(".header a");
 const menus = Array.from(document.getElementsByClassName("menuDiv"));
 const mobileWidth = window.matchMedia("(max-width: 450px)");
+const underlines = document.querySelectorAll(".underline");
 
 //mobileWidth.addEventListener("change", handleHeaders);
 //window.addEventListener("load", handleHeaders);
@@ -9,87 +10,59 @@ const mobileWidth = window.matchMedia("(max-width: 450px)");
 
 // mouseover also applies to child elements
 
-/*function handleHeaders() {
-    headers.forEach(header => {    
-        if (mobileWidth.matches) {
-            header.removeEventListener("mouseover", headerHoverHandler, true);
-            header.removeEventListener("mouseout", headerHoverHandler, true);
-        }
-        else {
-            header.addEventListener("mouseover", headerHoverHandler);
-            header.addEventListener("mouseout", leaveHeaderHandler);
-        }       
-    });
-}*/
-
-//function menuHoverHandler(e) {
-    //const listItems = Array.from(e.target.children);
-    //const dropdownHeight = listItems.length * 50;
-    //e.target.style.height = dropdownHeight + "px";
-    //e.target.previousElementSibling.firstElementChild.className = "hovered";
-//}
-
-/*function leaveMenuHandler(e) {
-    e.target.style.height = "0";
-    e.target.previousElementSibling.firstElementChild.children[0].classList.remove("hovered");
-}*/
-
-/*function headerHoverHandler(e) {
-    const listItems = Array.from(e.target.nextElementSibling.children);
-    const dropdownHeight = listItems.length * 50;
-    e.target.nextElementSibling.style.height = dropdownHeight + "px";
-    e.target.firstElementChild.classList.add("hovered");
-}*/
-
-/*function leaveHeaderHandler(e) {
-    e.target.firstElementChild.classList.remove("hovered");
-}*/
-
 document.addEventListener("mouseover", e => {
-    let selectedLink;
+    console.log(e.target);
+    let selectedLink;    
     if (e.target.matches(".header a")) {
-        selectedLink = e.target;
+        selectedLink = e.target;        
+        selectedLink.classList.add("hovered"); 
+        const selectedUnderline = selectedLink.nextElementSibling;
+        selectedUnderline.classList.add("hovered");
+        underlines.forEach(underline => {
+            if (underline !== selectedUnderline) {
+                underline.classList.remove("hovered");
+                underline.previousElementSibling.classList.remove("hovered");
+            }
+        });        
     }
-    console.log({selectedLink});
     let currentMenu;
     if (selectedLink) {
-        currentMenu = selectedLink.closest(".header").nextElementSibling;
-        console.log({currentMenu});
-        currentMenu.classList.add("active");
+        if (selectedLink.closest(".header").nextElementSibling.matches(".menuDiv")) {
+            currentMenu = selectedLink.closest(".header").nextElementSibling;  
+            currentMenu.classList.add("active");      
+        } 
         menus.forEach(menu => {
             if (menu === currentMenu) return;
             else {
-                menu.classList.remove("active");
-                menu.style.height = 0;
+                menu.style.height = 0;    
+                menu.classList.remove("active");                           
             }
         }); 
-        const listItems = Array.from(currentMenu.children);
+        const listItems = Array.from(currentMenu.children);        
         const dropdownHeight = listItems.length * 50;
-        currentMenu.style.height = dropdownHeight + "px";        
-        selectedLink.classList.add("hovered");        
-    }   
-    if (e.target.className)  
-   
-    console.log({currentMenu});
-});
+        currentMenu.style.height = dropdownHeight + "px";    
+        listItems.forEach(item => {            
+            item.addEventListener("mouseenter", e => {
+                console.log({item});
+                item.style.paddingLeft = "30px";
+            });
+            item.addEventListener("mouseleave", e => {
+                console.log({item});
+                item.style.paddingLeft = "0";
+            });
+        })       
+    }    
+    if (e.target.closest(".menuDiv.active") === null && e.target.closest(".header") === null) {
+        currentMenu = document.querySelector(".menuDiv.active");
+        currentMenu.style.height = 0;
+        links.forEach(link => {
+            link.className = "";
+            link.nextElementSibling.classList.remove("hovered");
+        });
+     }  
+}, true);
 
-/*function handleMenus() {
-    menus.forEach(menu => {
-        if (mobileWidth.matches) {            
-            menu.removeEventListener("mouseenter", menuHoverHandler, true);
-            menu.removeEventListener("mouseleave", menuHoverHandler, true);
-        }
-        else {
-            document.addEventListener("mouseover", e => {
-                if (e.target === menu || e.target.className === "header") {                
-                    menu.previousElementSibling.firstElementChild.children[1].classList.add("hovered");
-                }
-                else  menu.previousElementSibling.firstElementChild.children[1].classList.remove("hovered");
-            }, true);
-            menu.addEventListener("mouseenter", menuHoverHandler, true);
-            menu.addEventListener("mouseleave", leaveMenuHandler, true);            
-        }         
-    });
-}*/
+
+
 
 
